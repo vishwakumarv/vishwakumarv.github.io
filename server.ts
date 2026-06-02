179989f048b4685af1b48a2174bc76ca9b188bef
Vishwa Kumar <vishwakumarv05@gmail.com>
2026-06-02 12:51:05 +0530

    Fix lib imports for Vercel build

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
-import "./lib/error-capture";
+import "./src/lib/error-capture";
 
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
+import { consumeLastCapturedError } from "./src/lib/error-capture";
+import { renderErrorPage } from "./src/lib/error-page";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
